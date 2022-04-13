c1cd1d005e0e6fce3ad6249612f8aab9b6239370
ahtisam-t2k <ahtisam.bashir@travel2kashmir.com>
2022-04-13 17:46:43 +0530

    13-04-2022

--- next.config.js
+++ next.config.js
@@ -1,9 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   env:{
     serverURL:"http://103:136:36:27",
     port:"7860"
   }
 }
-module.exports = nextConfig
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/path*',
+        destination: 'http://103.136.36.27:7860/path*' // Proxy to Backend
+      }
+    ]
+  }
+}
--- pages/authorization/signin.js
+++ pages/authorization/signin.js
@@ -1,6 +1,8 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import Axios from "axios";
-import bcrypt from 'bcryptjs'
+import bcrypt from 'bcryptjs';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function Signin() {
 
@@ -10,17 +12,28 @@ function Signin() {
     "password": ''
   });
 
+  /** Storing Sign in data in Local Storage **/
+  const LocalSignin=({signinDetails}) =>
+    {
+        localStorage.setItem('Signin Details',JSON.stringify(signinDetails))
+    }
+
   /** Sign In Submit Function **/
   const submitSignIn = async () => {
     var item = {
       "user_email": signinDetails.email,
     }
-    Axios.post('/api/signin/user', item, { headers: { 'content-type': 'application/json' } })
+    const response = await fetch('/api/signin/user', {
+    method :'POST',
+    body : JSON.stringify({item}),
+    headers: { 'Content-Type': 'application/json' }})
       .then(
         async response => {
           const salt = response.data.salt
           const EncryptedPass = await bcrypt.hash(signinDetails.password, salt)
-          if (EncryptedPass === response.data.user_password) {
+          if
+           (EncryptedPass === response.data.user_password) {
+            /** Toast emitter Sign in Successfull **/
             toast.success(response?.data?.user_name + "  logged in ", {
               position: "top-center",
               autoClose: 5000,
@@ -39,7 +52,8 @@ function Signin() {
             Signin(whoIsLogged)
           }
           else {
-            toast.error("Invalid Password",
+            /** Toast emitter for error wrong email password combination  **/
+            toast.error("Please check your email and password",
               {
                 position: "top-center",
                 autoClose: 5000,
@@ -51,11 +65,11 @@ function Signin() {
               });
           }
         }
-
       )
       .catch(
         error => {
-          toast.error("Please check your email and password", {
+          /** Toast emitter for Sign in error  **/
+          toast.error("Sign in Error!", {
             position: "top-center",
             autoClose: 5000,
             hideProgressBar: false,
@@ -64,7 +78,6 @@ function Signin() {
             draggable: true,
             progress: undefined,
           });
-
         });
   }
 
@@ -81,6 +94,7 @@ function Signin() {
             <h2 className="text-2xl lg:text-3xl font-bold text-gray-700">
               Sign in
             </h2>
+             {/** Signin Form **/}
             <form className="mt-8 space-y-6" action="#">
               <div>
                 <label
@@ -156,7 +170,7 @@ function Signin() {
                 </a>
               </div>
               <button
-                type="submit" onClick={() => {submitSignIn()}}
+                type="submit" onClick={() => {submitSignIn();LocalSignin({signinDetails});}}
                 className="font-semibold text-white bg-cyan-600 
               hover:bg-cyan-700 focus:ring-4 focus:ring-cyan-200 mt-6
               rounded-lg text-base px-5 py-2 w-full sm:w-auto text-center"
@@ -173,6 +187,17 @@ function Signin() {
           </div>
         </div>
       </div>
+
+      {/** Toast Container **/}
+      <ToastContainer position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover />
     </div>
   );
 }

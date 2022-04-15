10ecbc591c81bcb944da1c6c600258957843491d
ahtisam-t2k <ahtisam.bashir@travel2kashmir.com>
2022-04-15 18:40:25 +0530

    15-04-2022

--- next.config.js
+++ next.config.js
@@ -13,5 +13,26 @@ module.exports = {
         destination: 'http://103.136.36.27:7860/:path*' // Proxy to Backend
       }
     ]
+  },
+  i18n:{
+    locales:['en-US', 'fr', 'nl-NL'],
+    defaultLocale:'en',
+    domains: [
+      {
+        domain: 'example.com',
+        defaultLocale: 'en-US',
+      },
+      {
+        domain: 'example.nl',
+        defaultLocale: 'nl-NL',
+      },
+      {
+        domain: 'example.fr',
+        defaultLocale: 'fr',
+        // an optional http field can also be used to test
+        // locale domains locally with http instead of https
+        http: true,
+      },
+    ],  
   }
 }

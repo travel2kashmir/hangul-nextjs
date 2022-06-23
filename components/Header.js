8fdfcafee1e6b4d8f3ce93b87a392ca2a95587bb
ahtisam-t2k <77894449+ahtisam-t2k@users.noreply.github.com>
2022-06-23 14:44:12 +0530

    23-06-2022

--- components/Header.js
+++ components/Header.js
@@ -170,7 +170,8 @@ function Navbar() {
                     <svg className="w-6 h-6 text-gray-500 flex-shrink-0 group-hover:text-gray-900 transition duration-75" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                     <path d="M7 3a1 1 0 000 2h6a1 1 0 100-2H7zM4 7a1 1 0 011-1h10a1 1 0 110 2H5a1 1 0 01-1-1zM2 11a2 2 0 012-2h12a2 2 0 012 2v4a2 2 0 01-2 2H4a2 2 0 01-2-2v-4z"></path>
                   </svg>
-                     <span className="ml-3 flex-1 whitespace-nowrap">{language?.services}</span>    
+                     <span className="ml-3 flex-1 whitespace-nowrap">
+                     <Link href="./services"><a>{language?.services}</a></Link></span>    
                 </li>
                 
                   <li className="text-base text-gray-900 font-normal rounded-lg flex items-center p-2 hover:bg-gray-100 group">
@@ -204,7 +205,7 @@ function Navbar() {
               0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2
                2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"></path></svg>
                   <span className="ml-3 flex-1 whitespace-nowrap">
-                  <Link href="/propertysummary"><a>{language?.propertysummary}</a></Link></span>
+                  <Link href="./propertysummary"><a>{language?.propertysummary}</a></Link></span>
                 </li>
                 <li className="text-base text-gray-900 font-normal rounded-lg flex items-center p-2 hover:bg-gray-100 group">
                   <svg className="w-6 h-6 text-gray-500 flex-shrink-0 group-hover:text-gray-900 transition duration-75" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M9 2a1 1 0 000 2h2a1 1 0 100-2H9z"></path><path fillRule="evenodd" d="M4 5a2 2 0 012-2 3 3 0 003 3h2a3 3 0 003-3 2 2 0 012 2v11a2 2 0 01-2 2H6a2 2 0 01-2-2V5zm3 4a1 1 0 000 2h.01a1 1 0 100-2H7zm3 0a1 1 0 000 2h3a1 1 0 100-2h-3zm-3 4a1 1 0 100 2h.01a1 1 0 100-2H7zm3 0a1 1 0 100 2h3a1 1 0 100-2h-3z" clipRule="evenodd"></path></svg>

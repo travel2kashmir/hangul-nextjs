ee1d51e0bf21eb3a3079d631d61ebcc7e0de7123
ahtisam-t2k <77894449+ahtisam-t2k@users.noreply.github.com>
2022-07-04 10:45:06 +0530

    04-07-2022

--- pages/property/contact.js
+++ pages/property/contact.js
@@ -337,7 +337,7 @@ const DeleteContact ={
                         </div>
                     </div>
                     <div className="flex items-center space-x-2 sm:space-x-3 ml-auto">
-                    <Button Primary={AddContact}  onClick={() => setView(1)}/>
+                    <Button Primary={language?.AddContact}  onClick={() => setView(1)}/>
                         <span className="w-1/2 text-gray-900 bg-white border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-cyan-200 font-semibold inline-flex items-center justify-center rounded-lg text-sm px-3 py-2 text-center sm:w-auto">
                             <svg className="-ml-1 mr-2 h-6 w-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M6 2a2 2 0 00-2 2v12a2 2 0 002 2h8a2 2 0 002-2V7.414A2 2 0 0015.414 6L12 2.586A2 2 0 0010.586 2H6zm5 6a1 1 0 10-2 0v3.586l-1.293-1.293a1 1 0 10-1.414 1.414l3 3a1 1 0 001.414 0l3-3a1 1 0 00-1.414-1.414L11 11.586V8z" clipRule="evenodd"></path></svg>
                            Import
@@ -426,11 +426,11 @@ const DeleteContact ={
                           </div>
                         </td>
                         <td className="p-4 whitespace-nowrap space-x-2">
-                        <Button Primary={EditContact}    onClick={() => {
+                        <Button Primary={language?.EditContact}    onClick={() => {
                               setUpdateContact(1);
                               setEditContact(item);
                             }}/>
-                          <Button Primary={DeleteContact}  onClick={() => {
+                          <Button Primary={language?.DeleteContact}  onClick={() => {
                               setDeleteContact(1);
                               setEditContact(item);
                             }}/>

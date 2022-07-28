042ee6fffb0282797c963c863df3e22def5d4a6b
sahil-t2k <92209390+sahil-t2k@users.noreply.github.com>
2022-07-28 11:52:26 +0530

    Update en.js

--- components/Languages/en.js
+++ components/Languages/en.js
@@ -1,11 +1,4 @@
 export default {
-  addimages:"Add Images",
-  addservices:"Add Services",
-  addroom:"Add Room",
-  addnewroom:"Add New Room",
-  addnewservices:"Add New Services",
-  addbasicdetails:"Add Basic Details",
-  page:"page",
   title: "sign in",
   email: "your email",
   password: "your password",
@@ -56,7 +49,6 @@ export default {
   licensing: "Licensing",
   cookiepolicy: "Cookie Policy",
   home: "Home",
-  Province: "Province",
   propertyname: "Property Name",
   propertycategory: "Property Category",
   propertybrand: "Property Brand",
@@ -89,88 +81,66 @@ export default {
   label:"Add",
    color: "bg-cyan-600 hover:bg-cyan-700 text-white ",
 },
- "AddContact" :{
-  label: "Add Contact",
-   color: "bg-cyan-600 hover:bg-cyan-700 text-white ",
-   icon:<svg className="-ml-1 mr-2 h-6 w-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clipRule="evenodd"></path></svg>
-   
-},
+ 
 
 
- "EditContact" :{
-label: "Edit Contact",
- color: "bg-cyan-600 hover:bg-cyan-700 text-white ",
- icon:<svg
- className="mr-2 h-5 w-5"
- fill="currentColor"
- viewBox="0 0 20 20"
- xmlns="http://www.w3.org/2000/svg"
->
- <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z"></path>
- <path
-   fillRule="evenodd"
-   d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z"
-   clipRule="evenodd"
- ></path>
-</svg>
+ 
+"ContactCols":{
+  name:"Contact",
+ col1 : "Contact Type",
+ col2 : "Contact Details", 
 },
-
-"DeleteContact" :{
-label: "Delete Contact",
- color: "bg-red-600 hover:bg-red-800 text-white ",
- icon: <svg
- className="mr-2 h-5 w-5"
- fill="currentColor"
- viewBox="0 0 20 20"
- xmlns="http://www.w3.org/2000/svg"
->
- <path
-   fillRule="evenodd"
-   d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-   clipRule="evenodd"
- ></path>
-</svg>
-},
-"EditMile" :{
-  label: "Edit Mile",
-   color: "bg-cyan-600 hover:bg-cyan-700 text-white ",
-   icon:<svg
-   className="mr-2 h-5 w-5"
-   fill="currentColor"
-   viewBox="0 0 20 20"
-   xmlns="http://www.w3.org/2000/svg"
-  >
-   <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z"></path>
-   <path
-     fillRule="evenodd"
-     d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z"
-     clipRule="evenodd"
-   ></path>
-  </svg>
-  },
   
-"DeleteMile" :{
-  label: "Delete Mile",
-   color: "bg-red-600 hover:bg-red-800 text-white ",
-   icon: <svg
-   className="mr-2 h-5 w-5"
-   fill="currentColor"
-   viewBox="0 0 20 20"
-   xmlns="http://www.w3.org/2000/svg"
-  >
-   <path
-     fillRule="evenodd"
-     d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-     clipRule="evenodd"
-   ></path>
-  </svg>
-  },
+"common":{
+Status:"Status",
+Action: "Actions",
+Edit: "Edit",
+Delete:"Delete",
+Add:"Add",
+Search:"Search",
+Previous:"Previous",
+Next:"Next",
+Showing: "Showing",
+Of:"of",
+Active:"Active",
+Inactive:"Inactive",
+Import:"Import",
+Save:"Save",
+Cancel:"Cancel"
+},
+"AdditionalServicesCols":{
+  name:"Additional Services",
+ col1 : "Service Name",
+ col2 : "Service Details", 
+},
 
-"AddImage":{
-  label: "Add image",
-  color: "bg-cyan-600 text-white  hover:bg-cyan-700",
-  icon: <svg className="-ml-1 mr-2 h-6 w-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clipRule="evenodd"></path></svg>
+"ServicesCols":{
+  name:"Services",
+ col1 : "Service Name",
+ col2 : "Service Details", 
+},
 
+"RoomCols":{
+  name:"Rooms",
+ col1 : "Room Name",
+ col2 : "Room Type", 
+},
+"PackageCols":{
+  name:"Packages",
+ col1 : "Package Name",
+  
+},
+"MilesCols":{
+  name:"Package Miles",
+ col1 : "Number of Miles",
+ col2 : "Miles  Provider",
+  
+},
+"EliteCols":{
+  name:"Elite Rewards",
+ col1 : "Program Name",
+ col2 : "Program Level",
+  
 },
 "Upload" :{
   label:"Upload",
@@ -396,7 +366,6 @@ label: "Delete Contact",
    icon:<svg className="-ml-1 mr-2 h-6 w-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clipRule="evenodd"></path></svg>
    
 },
-"addcontacts":"Add Contacts",
 "EditRoomBundle" :{
   label: "Edit Room Bundle",
    color: "bg-cyan-600 hover:bg-cyan-700 text-white ",
@@ -468,20 +437,7 @@ propertyxml:'../propertyxml',
 roomsxml:'../roomsxml',
 raterules:'../raterules'
  },
-
- "Sideadmin":{
-basicdetails:'./addbasicdetails',
-contact:'./addcontacts',
-gallery:'./addimages',
-reviews:'./addreviews',
-services: './addservices',
-rooms:'./addroom',
-packages:'../addpackage',
-roombundles:'./addroombundles',
-raterules:'../../property/addraterule',
-propertyxml:'../../property/propertyxml',
-roomsxml:'../../property/roomsxml'
-},
+ 
  "Side2":{
   basicdetails:'../../basicdetails',
   address:'../../address',

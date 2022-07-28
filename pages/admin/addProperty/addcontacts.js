4d02e66087c8785268c07a2d6a47105e3168db15
sahilHussain <sahil.hussain@travel2kashmir.com>
2022-07-28 09:49:43 +0530

    Update addcontacts.js

--- pages/admin/addProperty/addcontacts.js
+++ pages/admin/addProperty/addcontacts.js
@@ -14,7 +14,7 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 const logger = require("../../../services/logger");
 
-function addcontacts() {
+function Addcontacts() {
   const conTemp = {
     contact_type: '',
     contact_data: '',
@@ -343,4 +343,4 @@ useEffect(() => {
   )
 }
 
-export default addcontacts
+export default Addcontacts

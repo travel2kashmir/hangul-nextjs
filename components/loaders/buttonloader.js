36fac50e6af334350b317301a9c72f34afe9eac0
sahilHussain <sahil.hussain@travel2kashmir.com>
2022-08-24 14:41:24 +0530

    loader POC
    implemented loader in seperate folder in components and tested on basic details and images

---
+++ components/loaders/buttonloader.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function buttonloader() {
+    return (<>
+    
+    <div className="animate-pulse h-8 w-full bg-neutral-200 flex justify-end"></div>    
+   
+        
+          </>
+         )
+}
+
+export default buttonloader
---
+++ components/loaders/textboxloader.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function textboxloader(){
+  return <div className='my-4'>
+  <center><div className="animate-pulse h-36 bg-slate-200 rounded"></div></center>     
+    </div>
+}
+
+export default textboxloader

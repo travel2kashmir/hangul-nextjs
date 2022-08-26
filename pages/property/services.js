b8e56c7d751a59e07ad850a4b7259431f7736ef1
sahilHussain <sahil.hussain@travel2kashmir.com>
2022-08-26 10:15:18 +0530

    Update services.js

--- pages/property/services.js
+++ pages/property/services.js
@@ -70,9 +70,8 @@ function Services() {
                 setServices(response.data);
                 logger.info("url  to fetch property details hitted successfully")
                 setVisible(1)
-                console.log("response length" + JSON.stringify(response?.data.services))
                 if (response?.data.services === undefined) { Router.push('../admin/addProperty/addservices') }
-                setTimeout(() => console.log("fetched services" + services), 5000)
+               
                 {
                     response.data?.services?.map((item) => {
                         var temp = {
@@ -139,68 +138,34 @@ function Services() {
 
     }
 
-   
+
     return (
         <>
-        <div className={visible===0?'block':'hidden'}><Loader/></div>
-        <div className={visible===1?'block':'hidden'}>
-            <Header Primary={english?.Side} />
-            <Sidebar Primary={english?.Side} />
-            <div id="main-content"
-                className="  bg-white pt-24 relative overflow-y-auto lg:ml-64">
-                {/* Navbar */}
-                <nav className="flex mb-5 ml-4" aria-label="Breadcrumb">
-                    <ol className="inline-flex items-center space-x-1 md:space-x-2">
-                        <li className="inline-flex items-center">
-                            <svg
-                                className="w-5 h-5 mr-2.5"
-                                fill="currentColor"
-                                viewBox="0 0 20 20"
-                                xmlns="http://www.w3.org/2000/svg"
-                            >
-                                <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path>
-                            </svg>
-                            <Link
-                                href="./landing"
-                                className="text-gray-700 text-base font-medium hover:text-gray-900 inline-flex items-center"
-                            >
-                                <a>{language?.home}</a>
-                            </Link>
-                        </li>
-                        <li>
-                            <div className="flex items-center">
-                                <svg
-                                    className="w-6 h-6 text-gray-400"
-                                    fill="currentColor"
-                                    viewBox="0 0 20 20"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                >
-                                    <path
-                                        fillRule="evenodd"
-                                        d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                                        clipRule="evenodd"
-                                    ></path>
-                                </svg>
-                                <span className="text-gray-700 text-sm capitalize  font-medium hover:text-gray-900 ml-1 md:ml-2">
-                                    <Link href="./propertysummary" >
-                                        <a> {services?.property_name}</a>
-                                    </Link></span>
-                            </div>
-                        </li>
-                        <li>
-                            <div className="flex items-center">
+            <div className={visible === 0 ? 'block' : 'hidden'}><Loader /></div>
+            <div className={visible === 1 ? 'block' : 'hidden'}>
+                <Header Primary={english?.Side} />
+                <Sidebar Primary={english?.Side} />
+                <div id="main-content"
+                    className="  bg-white pt-24 relative overflow-y-auto lg:ml-64">
+                    {/* Navbar */}
+                    <nav className="flex mb-5 ml-4" aria-label="Breadcrumb">
+                        <ol className="inline-flex items-center space-x-1 md:space-x-2">
+                            
+                            <li>
+                                <div className="flex items-center">
 
-                                <svg
-                                    className="w-5 h-5 mr-2.5"
-                                    fill="currentColor"
-                                    viewBox="0 0 20 20"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                >
-                                    <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path>
-                                </svg>
+                                    <svg
+                                        className="w-5 h-5 mr-2.5"
+                                        fill="currentColor"
+                                        viewBox="0 0 20 20"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                    >
+                                        <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path>
+                                    </svg>
 
-                                <Link href={currentLogged?.id.match(/admin.[0-9]*/) ? "../admin/AdminLanding" : "./landing"} className="text-gray-700 text-base font-medium hover:text-gray-900 inline-flex items-center"><a>{language?.home}</a>
-                                </Link>
+                                    <Link href={currentLogged?.id.match(/admin.[0-9]*/) ? "../admin/AdminLanding" : "./landing"} className="text-gray-700 text-base font-medium hover:text-gray-900 inline-flex items-center"><a>{language?.home}</a>
+                                    </Link>
+                                </div>
                             </li>
                             <li>
                                 <div className="flex items-center">
@@ -263,34 +228,6 @@ function Services() {
                 </div>
 
             </div>
-
-                                <span
-                                    className="text-gray-400 ml-1 md:ml-2 font-medium text-sm  "
-                                    aria-current="page"
-                                >
-                                    {language?.services}
-                                </span>
-                            </div>
-                        </li>
-                    </ol>
-                </nav>
-
-               <Table  gen={gen} setGen={setGen}   
-                edit={updateServices}  common={language?.common} cols={language?.ServicesCols}
-                name="Services"/> 
-              {/* Toast Container */}
-                <ToastContainer position="top-center"
-                    autoClose={5000}
-                    hideProgressBar={false}
-                    newestOnTop={false}
-                    closeOnClick
-                    rtl={false}
-                    pauseOnFocusLoss
-                    draggable
-                    pauseOnHover />
-            </div>
-        </div>
-
         </>
     )
 }

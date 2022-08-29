1b278f63e2641f8ef75d7691956b28deb831ec1e
sahilHussain <sahil.hussain@travel2kashmir.com>
2022-08-29 14:20:35 +0530

    implemented the dynamic link through out

--- pages/property/allroombundles/addroombundles.js
+++ pages/property/allroombundles/addroombundles.js
@@ -16,6 +16,7 @@ import Header from '../../../components/Header'
 var language;
 var currentProperty;
 var currentPackageDetails;
+var currentLogged;
 var rateId;
 const logger = require("../../../services/logger");
 
@@ -45,9 +46,8 @@ function Addroombundles() {
         }
         /** Current Property Basic Details fetched from the local storage **/
         currentProperty = JSON.parse(localStorage.getItem("property"));
-        currentPackageDetails = JSON.parse(
-        localStorage.getItem("packageDescription")
-        );
+        currentPackageDetails = JSON.parse(localStorage.getItem("packageDescription"));
+        currentLogged = JSON.parse(localStorage.getItem("Signin Details"));
       }
     };
     firstfun();
@@ -168,21 +168,11 @@ function Addroombundles() {
     >
       <nav className="flex mb-5 ml-4" aria-label="Breadcrumb">
         <ol className="inline-flex items-center space-x-1 md:space-x-2">
-          <li className="inline-flex items-center">
-            <svg
-              className="w-5 h-5 mr-2.5"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path>
-            </svg>
-            <span className="text-gray-700 text-base font-medium hover:text-gray-900 inline-flex items-center">
-              <Link href="../landing">
-                <a> {language?.home}</a>
-              </Link>
-            </span>
-          </li>
+        <li className="inline-flex items-center">
+                <svg className="w-5 h-5 mr-2.5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path></svg>
+                <Link href={currentLogged?.id.match(/admin.[0-9]*/) ? "../../admin/AdminLanding" : "../landing"} className="text-gray-700 text-base font-medium hover:text-gray-900 inline-flex items-center"><a>{language?.home}</a>
+                </Link>
+              </li>
           <li>
             <div className="flex items-center">
               <svg
--- pages/property/raterules/addraterule.js
+++ pages/property/raterules/addraterule.js
@@ -13,6 +13,7 @@ import french from '../../../components/Languages/fr'
 import arabic from '../../../components/Languages/ar'
 import axios from "axios";
 import Router from 'next/router';
+var currentLogged;
 var i = 0;
 var j = 1;
 var res =[]
@@ -76,6 +77,7 @@ function Addraterule() {
             currentraterule = localStorage.getItem('RateRuleId');
             /** Current Property Details fetched from the local storage **/
             currentProperty = JSON.parse(localStorage.getItem("property"));
+            currentLogged = JSON.parse(localStorage.getItem("Signin Details"));
          }
         }
         firstfun();
@@ -665,22 +667,13 @@ function Addraterule() {
         {/* Navbar */}
         <nav className="flex mb-5 ml-4" aria-label="Breadcrumb">
           <ol className="inline-flex items-center space-x-1 md:space-x-2">
-            <li className="inline-flex items-center">
-              <svg
-                className="w-5 h-5 mr-2.5"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path>
-              </svg>
-              <Link
-                href="../landing"
-                className="text-gray-700 text-base font-medium hover:text-gray-900 inline-flex items-center"
-              >
-                <a>{language?.home}</a>
-              </Link>
-            </li>
+            
+          <li className="inline-flex items-center">
+                <svg className="w-5 h-5 mr-2.5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path></svg>
+                <Link href={currentLogged?.id.match(/admin.[0-9]*/) ? "../../admin/AdminLanding" : "../landing"} className="text-gray-700 text-base font-medium hover:text-gray-900 inline-flex items-center"><a>{language?.home}</a>
+                </Link>
+              </li>
+          
             <li>
               <div className="flex items-center">
                 <svg

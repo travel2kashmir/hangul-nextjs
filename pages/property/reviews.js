f20f8e378fd32384e4fc84636aae228443983a14
sahilHussain <sahil.hussain@travel2kashmir.com>
2022-09-05 18:00:14 +0530

    reviews under progress

--- pages/property/reviews.js
+++ pages/property/reviews.js
@@ -24,11 +24,12 @@ function Reviews() {
   const [visible,setVisible]=useState(0);
   const [view,setView]=useState(0);
   const [del,setDel]=useState('');
+  const [modelDel,setModelDel]=useState(0)
   const delConfirm =(id)=>{
-    alert(id)
-    var url=`/api/${id}`;
-    alert(id);
-    axios.delete(`/api/${id}`).then(()=>{fetchReviews(); alert(`${del} deleted sucessfully`);}).catch(()=>alert("some error occured"))
+    alert(del)
+    var url=`/api/${del}`;
+    alert(url);
+    axios.delete(`${url}`).then(()=>{fetchReviews(); alert(`${del} deleted sucessfully`); setModelDel(0)}).catch(()=>alert("some error occured"))
   }
   
   useEffect(()=>{  
@@ -248,7 +249,7 @@ function Reviews() {
                                                     fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z"></path><path fillRule="evenodd" d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clipRule="evenodd"></path></svg>
                                             </button>
                                             <button
-                                                onClick={() => { alert(item?.review_id); setDel(item?.review_id); delConfirm(item?.review_id) }} className={currentLogged.id.match(/admin.[0-9]*/)?`text-gray-500   ml-4 mr-2 hover:text-gray-900 
+                                                onClick={() => { setDel(item?.review_id); setModelDel(1);  }} className={currentLogged.id.match(/admin.[0-9]*/)?`text-gray-500   ml-4 mr-2 hover:text-gray-900 
                                                 cursor-pointer hover:bg-gray-100 rounded `:'hidden'}>
                                                 <svg className="  w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clipRule="evenodd"></path></svg>
                                             </button>
@@ -280,6 +281,23 @@ function Reviews() {
             ))}
         </div>
     </div>
+    {/*model del */}
+    <div className={modelDel===1?'block':'hidden'}>
+    <div className="overflow-x-hidden overflow-y-auto fixed top-4 left-0 right-0 backdrop-blur-xl bg-black/30 md:inset-0 z-50 flex justify-center items-center h-modal sm:h-full">
+            <div className="relative w-full max-w-2xl px-4 h-full md:h-auto">
+              <div className="bg-white rounded-lg shadow relative m-4 px-4 py-6">
+                <span className="mx-4 capitalize bold">Are you sure you want to delete this review?</span><br/>
+                <button onClick={()=>delConfirm()} className='m-2 bg-gradient-to-r bg-green-600 hover:bg-green-700 text-white  sm:inline-flex 
+                                                           font-semibold rounded-lg text-sm px-5 py-2 text-center items-center ease-linear transition-all
+                                                            duration-150'>Yes</button>
+                <button onClick={()=>setModelDel(0)} className='ml-4 mt-2 bg-gradient-to-r bg-red-600 hover:bg-red-700 text-white  sm:inline-flex 
+                                                           font-semibold rounded-lg text-sm px-5 py-2 text-center items-center ease-linear transition-all
+                                                            duration-150'>No</button>
+
+    </div></div></div></div>
+
+
+
       {/* Modal Add */}
       <div className={view === 1 ? "block" : "hidden"}>
           <div className="overflow-x-hidden overflow-y-auto fixed top-4 left-0 right-0 backdrop-blur-xl bg-black/30 md:inset-0 z-50 flex justify-center items-center h-modal sm:h-full">

ade176639a48dee26ccdb2102f7c3823d64db48a
sahilHussain <sahil.hussain@travel2kashmir.com>
2022-09-09 18:24:04 +0530

    template v1

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,7 @@ import 'nprogress/nprogress.css';
 import Layout from '../components/Layout'
 import Router from "next/router";
 
+
 Router.events.on('routeChangeStart', () => NProgress.start());
 Router.events.on('routeChangeComplete', () => NProgress.done());
 Router.events.on('routeChangeError', () => NProgress.done());

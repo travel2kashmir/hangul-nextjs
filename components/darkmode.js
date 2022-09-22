a73e7953810efd2c6ee476522465fd567b5d66b8
ahtisam-t2k <77894449+ahtisam-t2k@users.noreply.github.com>
2022-09-22 13:24:34 +0530

    22-09-2022

---
+++ components/darkmode.js
@@ -0,0 +1,13 @@
+export default {
+    "light_mode": {
+        text: "text-grey-700",
+        greybackground: "bg-gray-50",
+        whitebackground:"bg-white"
+    },
+    "dark_mode": {
+        text: "text-white",
+        greybackground: "bg-gray-900",
+        whitebackground:"bg-gray-800"
+    }
+
+}
